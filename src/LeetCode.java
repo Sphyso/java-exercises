27b2445935f6c328ff81ecafd2fb86650afc51b8
Sifiso <sifisohadebe0103@gmail.com>
2026-01-06 22:08:33 +0200

    Changed approach

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3,22 +3,13 @@
 public class LeetCode {
     public static void main(String[] args) {
         int[] digits = {1,2,9};
-        int lastNum = digits[digits.length - 1];
-        int test = 0;
-        // Check if last is nine
-        if (lastNum != 9){
-            int[] newDigits = new int[digits.length];
-            test = digits.length;
-        }else{
-            int[] newDigits = new int[digits.length+1];
-            test = digits.length+1;
-        }
+        String numberString = "";
         int num = 0;
 
         for (int x = 0; x < digits.length; x++){
-            num += digits[x];
+            numberString += Integer.toString(digits[x]);
         }
-        System.out.println(test);
+        System.out.println(numberString);
     }
 
     // 66. Plus One
