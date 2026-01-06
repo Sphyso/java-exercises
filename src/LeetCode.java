dd39ec3d9d165baf63b24540ec30f0692fc66e1f
Sifiso <sifisohadebe0103@gmail.com>
2026-01-06 23:02:27 +0200

    Able to convert to int, add one and convert back to string

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,13 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class LeetCode {
     public static void main(String[] args) {
-        int[] digits = {1,2,9};
+        // used to convert to a string
+        int[] digits = {1,2,3};
         String numberString = "";
-        int num = 0;
 
+        // convert to a string so I can add one
         for (int x = 0; x < digits.length; x++){
             numberString += Integer.toString(digits[x]);
         }
+
+        // convert str to num and add one
+        // then convert back
+        int number = Integer.parseInt(numberString);
+        number += 1;
+        numberString = "";
+        numberString += Integer.toString(number);
+
+
         System.out.println(numberString);
     }
 
