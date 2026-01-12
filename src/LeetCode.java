31b90a8d49cdaa8c1d931e64d2462cf3d3276cf8
Sifiso <sifisohadebe0103@gmail.com>
2026-01-12 21:03:22 +0200

    Struggling with larger numbers

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,7 +1,9 @@
+import  java.math.BigInteger;
+
 public class LeetCode {
     public static void main(String[] args) {
         // used to convert to a string
-        int[] digits = {1,2,3};
+        int[] digits = {9,8,7,6,5,4,3,2,1,0};
 
         LeetCode lc = new LeetCode();
         lc.plusOne(digits);
@@ -10,7 +12,6 @@ public class LeetCode {
 
     // 66. Plus One
     public int[] plusOne(int[] digits) {
-
         String numberString = "";
 
         // convert to a string so I can add one
@@ -20,17 +21,19 @@ public class LeetCode {
 
         // convert str to num and add one
         // then convert back
-        int number = Integer.parseInt(numberString);
+        // int not ideal, double returns scientific notation
+        //long number = Long.parseLong(numberString);
+        BigInteger number = new BigInteger(numberString);
         number += 1;
         numberString = "";
-        numberString += Integer.toString(number);
+        numberString += Long.toString(number);
 
         // returned array with new length
         int[] newDigits = new int[numberString.length()];
 
         for (int x = 0; x < numberString.length(); x++){
             newDigits[x] = Character.getNumericValue(numberString.charAt(x));
-            //System.out.println(newDigits[x]);
+            System.out.println(newDigits[x]);
         }
 
         return newDigits;
